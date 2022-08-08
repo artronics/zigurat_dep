0d576aebc418f27d84e1a981168de9cbcb065e3d
Jalal Hosseini <jalalhosseiny@gmail.com>
2022-08-09 00:11:04 +0100

    add Template struct

--- src/lib.zig
+++ src/lib.zig
@@ -1,6 +1,49 @@
 const std = @import("std");
 const testing = std.testing;
+const Allocator = std.mem.Allocator;
 const expect = testing.expect;
+
+pub fn Template() type {
+    return struct {
+        alloc: Allocator,
+        path: []const u8,
+        buf: []u8,
+
+        const Self = @This();
+
+        pub fn init(allocator: Allocator, path: []const u8) !Self {
+            var v = try allocator.alloc(u8, 10);
+
+            return Self{ .alloc = allocator, .path = path, .buf = v };
+        }
+        pub fn deinit(self: Self) void {
+            self.alloc.free(self.buf);
+        }
+    };
+}
+
+test "init" {
+    var s = try Template().init(testing.allocator, "yo");
+    _ = try std.fs.cwd().openFile("./zig-out/bin/yo", .{});
+    defer s.deinit();
+    try expect(s.path[0] == 'y');
+}
+
+test "path" {
+    var buf:[100]u8 = undefined;
+    var s: []u8 = buf[0..];
+    const ff = try std.fs.cwd().realpath("./zig-out/bin/yo", s);
+    std.log.warn("abs {s}", .{ff}); // get the abs part from rel part
+
+    try expect(std.fs.path.isAbsolute(ff));
+    try expect(!std.fs.path.isAbsolute("./zig-out/bin/yo"));
+
+    const d = std.fs.path.dirname("./zig-out/bin").?; // not working; strip out the /bin part
+    std.log.warn("dirname {s}", .{d});
+    const kk = try std.fs.createFileAbsolute(ff, .{});
+    const md = try kk.metadata();
+    std.log.warn("dirname2 {s}", .{@tagName(md.kind())}); // File
+}
 
 test "file size" {
     var tmp_dir = testing.tmpDir(.{}); // This creates a directory under ./zig-cache/tmp/{hash}/test_file
